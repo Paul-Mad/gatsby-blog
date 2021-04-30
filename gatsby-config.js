f1d4b462f1544c8aa86a2605f0309b77b5f0effd
Paul-Mad <paulin.mad@hotmail.com>
2021-04-30 18:03:21 +0100

    adding simple blog code for gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components``gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
